02e50ea7ab24ccf1b4503d4f2df1eec795600c87
Gilbert Yang <gilbertyang@outlook.com>
2025-02-09 15:51:21 -0500

    improve help info

--- README.md
+++ README.md
@@ -23,14 +23,45 @@ Average Degree: (3 + 2 + 2 +2 + 1) edges / 6 nodes = ~1.67
 Follow the instruction on [Maven installation guide](https://maven.apache.org/install.html).
 
 #### Mac:
-```brew install maven```
+```
+brew install maven
+```
 
 #### Test maven installation
-```mvn -v```
-
+```
+mvn -v
+```
 
 ### Run the project
-```
-mvn clean compile install test
 
 ```
+mvn clean package
+java -cp target/graph-1.0-SNAPSHOT.jar com.example.gretel.tweets.GraphCalculator
+
+```
+
+## How to use?
+
+There are 7 commands you can use when launch the calculator.
+
+### init
+This is the command to initialize the tweet graph. By default it will use the file under `src/main/java/com/example/gretel/static/tweets.txt`.
+
+
+### add
+This is the command to add new tweet. For simplicity, the input here is `#tag1,#tag2,#tag3`. 
+
+### remove
+This is the command to remove existing tweet. If there is no such tweet, it will return false. For simplicity, the input here is `#tag1,#tag2,#tag3`. 
+
+### list-all
+This is the command to list all existing tweets. By default it will show the first 100 tweets.
+
+### avg
+This is the command to calculate the avarage degree for the current graph.
+
+### help
+This is the commmand to show help information about how to use this calculator.
+
+### exit
+This is the command to exit the calculator.
--- src/main/java/com/example/gretel/tweets/GraphCalculator.java
+++ src/main/java/com/example/gretel/tweets/GraphCalculator.java
@@ -4,15 +4,12 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Stream;
@@ -22,7 +19,7 @@ public class GraphCalculator {
 
     public static void initGraphCmd(Scanner scanner) {
     
-        System.out.println("Enter the path to the file:");
+        System.out.println("Enter the path to the file, if you input nothing, it will use the default input in src/main/java/com/example/gretel/static/tweets.txt:");
         String path = scanner.nextLine().trim();
         if (path.isEmpty()) {
             path = "src/main/java/com/example/gretel/static/tweets.txt";
@@ -138,7 +135,6 @@ public class GraphCalculator {
         // graph.getNodesCount().forEach((tag, count) ->
         //         System.out.println(tag + " : " + count + " occurrences"));
         int lines = Integer.parseInt(input);
-        saveMap(graph.getEdgeMap());
         System.out.println("\nEdges (Co-occurring Hashtags):");
         graph.getEdgeMap().entrySet()
             .stream()
@@ -147,22 +143,11 @@ public class GraphCalculator {
 
     }
 
-    public static void saveMap(Map<String, Integer> map) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("map2.csv"))) {
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                writer.println(entry.getKey() + "," + entry.getValue());
-            }
-            System.out.println("Map saved as CSV!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("\nEnter a command (init, add, remove, list-all, avg, help, exit):");
+            System.out.println("\nEnter a command (init, add, remove, list-all, avg, help, exit), if you don't know how to use, type help:");
             String command = scanner.nextLine().trim().toLowerCase();
 
             switch (command) {
@@ -183,7 +168,21 @@ public class GraphCalculator {
                     System.out.printf("Average Degree: %.3f%n", graph.getAverageDegree());
                     break;
                 case "help":
-                    System.out.println("Commands: init: initialize graph, add: add tweet, remove: remove tweet, listAll: list graph, avg: calculate average degree, exit: exit program");
+                    System.out.println("\nAvailable Commands:");
+                    System.out.println("--------------------------------------------------");
+                    System.out.println("init\t\t\t\t\t\t\t\t- Initialize the graph (default path: src/main/java/com/example/gretel/static/tweets.txt)");
+                    System.out.println("add <tweet_tags>\t\t\t\t\t\t\t- Add a new tweet to the graph");
+                    System.out.println("remove <tweet_tags>\t\t\t\t\t- Remove a tweet from the graph using its ID");
+                    System.out.println("list-all\t\t\t\t\t\t\t- List all tweets currently stored in the graph");
+                    System.out.println("avg\t\t\t\t\t\t\t\t\t- Calculate and display the average degree of the graph");
+                    System.out.println("exit\t\t\t\t\t\t\t\t- Exit the application");
+                    System.out.println("\nExample usage:");
+                    System.out.println("- To initialize the graph: init");
+                    System.out.println("- To add a tweet tags: 1.add 2.#data,#gretel");
+                    System.out.println("- To remove a tweet tags : 1.remove 2.#data,#gretel");
+                    System.out.println("- To list all tweets: list-all");
+                    System.out.println("- To calculate the average degree: avg");
+                    System.out.println("- To exit: exit");
                     break;
                 case "exit":
                     System.out.println("Exiting...");
